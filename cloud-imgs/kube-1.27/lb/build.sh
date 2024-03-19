a792d1127b7b85d59e652b97ca309823e4e83a61
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-03-19 08:18:22 +0700

    cloud-imgs: change base img to jammy

--- cloud-imgs/kube-1.27/lb/build.sh
+++ cloud-imgs/kube-1.27/lb/build.sh
@@ -6,10 +6,10 @@ if [ -z $TEMPLATE_VM_ID ]; then
 fi
 
 # SECTION: build os image
-BASE_IMAGE_FILE=bionic-server-cloudimg-amd64.img
-IMAGE_FILE=haproxy.img
+BASE_IMAGE_FILE=ubuntu.img
+IMAGE_FILE=kube-lb.img
 if [ ! -f $BASE_IMAGE_FILE ]; then
-    wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img -O $BASE_IMAGE_FILE
+    wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img -O $BASE_IMAGE_FILE
 fi
 rm -f $IMAGE_FILE
 cp $BASE_IMAGE_FILE $IMAGE_FILE
@@ -19,7 +19,7 @@ virt-customize -a $IMAGE_FILE --copy-in config_haproxy.py:/usr/local/bin/
 virt-customize -a $IMAGE_FILE --run userdata.sh # Optional
 
 # SECTION: create template vm
-IMAGE_FILE=haproxy.img
+IMAGE_FILE=kube-lb.img
 TEMPLATE_VM_CORE_COUNT=2
 TEMPLATE_VM_MEM=2048
 STORAGE=local # TODO: your storage name
@@ -29,5 +29,5 @@ qm set $TEMPLATE_VM_ID --scsi0 $STORAGE:0,import-from=$PWD/$IMAGE_FILE
 qm set $TEMPLATE_VM_ID --ide2 $STORAGE:cloudinit
 qm set $TEMPLATE_VM_ID --boot order=scsi0
 qm set $TEMPLATE_VM_ID --serial0 socket --vga serial0
-qm set $TEMPLATE_VM_ID --name haproxy
+qm set $TEMPLATE_VM_ID --name kube-lb
 qm template $TEMPLATE_VM_ID
--- cloud-imgs/kube-1.27/node/build.sh
+++ cloud-imgs/kube-1.27/node/build.sh
@@ -6,10 +6,10 @@ if [ -z $TEMPLATE_VM_ID ]; then
 fi
 
 # SECTION: build os image
-BASE_IMAGE_FILE=bionic-server-cloudimg-amd64.img
-IMAGE_FILE=kube.img
+BASE_IMAGE_FILE=ubuntu.img
+IMAGE_FILE=kube-node.img
 if [ ! -f $BASE_IMAGE_FILE ]; then
-    wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img -O $BASE_IMAGE_FILE
+    wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img -O $BASE_IMAGE_FILE
 fi
 rm -f $IMAGE_FILE
 cp $BASE_IMAGE_FILE $IMAGE_FILE
@@ -20,7 +20,8 @@ virt-customize -a $IMAGE_FILE --copy-in $PWD/containerd/config.toml:/etc/contain
 virt-customize -a $IMAGE_FILE --install nfs-common
 
 # SECTION: create vm template
-IMAGE_FILE=kube.img
+IMAGE_FILE=kube-node.img
+KUBE_VERSION=1.27
 TEMPLATE_VM_CORE_COUNT=4
 TEMPLATE_VM_MEM=8192
 STORAGE=local # TODO: your storage name
@@ -30,5 +31,5 @@ qm set $TEMPLATE_VM_ID --scsi0 $STORAGE:0,import-from=$PWD/$IMAGE_FILE
 qm set $TEMPLATE_VM_ID --ide2 $STORAGE:cloudinit
 qm set $TEMPLATE_VM_ID --boot order=scsi0
 qm set $TEMPLATE_VM_ID --serial0 socket --vga serial0
-qm set $TEMPLATE_VM_ID --name kube-1.27
+qm set $TEMPLATE_VM_ID --name kube-node-$KUBE_VERSION
 qm template $TEMPLATE_VM_ID
