1ffbfde550a014fc2efc3b7b5bed194027e228fd
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-03-21 17:36:47 +0700

    test: build img

--- cloud-imgs/kube-1.27/node/build.sh
+++ cloud-imgs/kube-1.27/node/build.sh
@@ -14,7 +14,8 @@ if [ ! -f $BASE_IMAGE_FILE ]; then
     wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img -O $BASE_IMAGE_FILE
 fi
 rm -f $IMAGE_FILE
-qemu-img create -f qcow2 $IMAGE_FILE 20G
+cp $BASE_IMAGE_FILE $IMAGE_FILE
+qemu-resize $IMAGE_FILE +20G
 virt-resize --format qcow2 --expand /dev/sda1 $BASE_IMAGE_FILE $IMAGE_FILE
 virt-customize -a $IMAGE_FILE --install qemu-guest-agent
 virt-customize -a $IMAGE_FILE --run userdata.sh
